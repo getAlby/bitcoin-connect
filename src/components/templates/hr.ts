0c7db0c79898a572945e7f26abb25c01f140902b
Roland Bewick <roland.bewick@gmail.com>
2023-10-18 14:29:28 +0700

    fix: hr border color

--- src/components/templates/hr.ts
+++ src/components/templates/hr.ts
@@ -1,5 +1,8 @@
 import {html} from 'lit';
+import {classes} from '../css/classes';
 
 export function hr() {
-  return html`<hr class="border-t w-full border-neutral-secondary" />`;
+  return html`<hr
+    class="border-t w-full ${classes['border-neutral-secondary']}"
+  />`;
 }
