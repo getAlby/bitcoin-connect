202fef87a98ffa3d6e2262e3f09c0cf881df6d2c
Roland Bewick <roland.bewick@gmail.com>
2023-11-10 21:58:55 +0700

    fix: use browser locale for currency

--- src/components/bc-start.ts
+++ src/components/bc-start.ts
@@ -56,7 +56,7 @@ export class Start extends withTwind()(BitcoinConnectElement) {
                 class="font-bold font-mono text-4xl align-bottom ${classes[
                   'text-brand-mixed'
                 ]}"
-                >${(this._balance || 0).toLocaleString('en', {
+                >${(this._balance || 0).toLocaleString(undefined, {
                   useGrouping: true,
                 })}</span
               >&nbsp;sats
