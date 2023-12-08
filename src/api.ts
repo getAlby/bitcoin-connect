1bf59fd39fd8472d24eec234befc288fd4ff2faf
Roland Bewick <roland.bewick@gmail.com>
2023-12-08 19:32:44 +0700

    chore: document API functions

--- src/api.ts
+++ src/api.ts
@@ -3,16 +3,37 @@ import store from './state/store';
 import {ConnectorFilter} from './types/ConnectorFilter';
 
 type BitcoinConnectConfig = {
+  /**
+   * Name of the application that the user is interacting with.
+   *   May be passed to the connector the user chooses to connect with (e.g. NWC)
+   */
   appName?: string;
+  /**
+   * Limit which connectors are shown in the connect flow
+   */
   filters?: ConnectorFilter[];
+  /**
+   * Set to false to not request or show the user's wallet balance
+   */
   showBalance?: boolean;
 };
 
 type LaunchModalArgs = {
+  /**
+   * Launch a payment flow to pay a BOLT11 invoice
+   */
   invoice?: string;
+  /**
+   * @param response response of the WebLN send payment call
+   */
   onPaid?: (response: SendPaymentResponse) => void;
 };
 
+/**
+ * Listen to onConnected events which will fire when a user connects to a wallet
+ * @param callback includes the webln provider that was connected
+ * @returns unsubscribe function
+ */
 export function onConnected(callback: (provider: WebLNProvider) => void) {
   const zustandUnsubscribe = store.subscribe(async (state, prevState) => {
     if (state.connected && !prevState.connected) {
@@ -27,6 +48,11 @@ export function onConnected(callback: (provider: WebLNProvider) => void) {
   };
 }
 
+/**
+ * Listen to onConnecting events which will fire when a user is connecting to their wallet
+ * @param callback
+ * @returns unsubscribe function
+ */
 export function onConnecting(callback: () => void) {
   const zustandUnsubscribe = store.subscribe(async (state, prevState) => {
     if (state.connecting && !prevState.connecting) {
@@ -38,6 +64,11 @@ export function onConnecting(callback: () => void) {
   };
 }
 
+/**
+ * Listen to onDisconnected events which will fire when a user disconnects from their wallet
+ * @param callback
+ * @returns unsubscribe function
+ */
 export function onDisconnected(callback: () => void) {
   const zustandUnsubscribe = store.subscribe(async (state, prevState) => {
     if (!state.connected && prevState.connected) {
@@ -49,6 +80,11 @@ export function onDisconnected(callback: () => void) {
   };
 }
 
+/**
+ * Listen to onModalOpened events which will fire when a Bitcoin Connect modal is opened
+ * @param callback
+ * @returns unsubscribe function
+ */
 export function onModalOpened(callback: () => void) {
   const zustandUnsubscribe = store.subscribe(async (state, prevState) => {
     if (state.modalOpen && !prevState.modalOpen) {
@@ -60,6 +96,11 @@ export function onModalOpened(callback: () => void) {
   };
 }
 
+/**
+ * Listen to onModalOpened events which will fire when a Bitcoin Connect modal is closed
+ * @param callback
+ * @returns unsubscribe function
+ */
 export function onModalClosed(callback: () => void) {
   const zustandUnsubscribe = store.subscribe(async (state, prevState) => {
     if (!state.modalOpen && prevState.modalOpen) {
@@ -71,6 +112,13 @@ export function onModalClosed(callback: () => void) {
   };
 }
 
+/**
+ * If a WebLN provider already exists, returns the current WebLN provider. Otherwise
+ *   will launch the modal to allow the user to connect to a wallet,
+ *   and then enable the WebLN provider for that wallet.
+ * @returns an enabled WebLN provider.
+ * @throws Error if user cancels flow (by closing the modal)
+ */
 export async function requestProvider(): Promise<WebLNProvider> {
   let provider = store.getState().provider;
 
@@ -97,16 +145,27 @@ export async function requestProvider(): Promise<WebLNProvider> {
   return provider;
 }
 
+/**
+ * @returns true if user is connected to a wallet and WebLN is enabled
+ */
 export function isConnected() {
   return store.getState().connected;
 }
 
+/**
+ * Configures Bitcoin Connect
+ * @param config
+ */
 export function init(config: BitcoinConnectConfig = {}) {
   store.getState().setAppName(config.appName);
   store.getState().setFilters(config.filters);
   store.getState().setShowBalance(config.showBalance);
 }
 
+/**
+ * Programmatically launch the Bitcoin Connect modal
+ * @param args optionally configure the modal e.g. to launch a payment flow
+ */
 export function launchModal({invoice, onPaid}: LaunchModalArgs = {}) {
   const existingModal = document.querySelector('bc-modal');
   if (existingModal) {
@@ -115,6 +174,7 @@ export function launchModal({invoice, onPaid}: LaunchModalArgs = {}) {
 
   // TODO: refactor to have higher level components that render these ones,
   // so JS DOM functions are not needed and tailwind can be used
+  // (also then CSS is not needed in the host css of bc-send-payment-flow and bc-connect-flow)
 
   const modalElement = document.createElement('bc-modal');
 
@@ -146,10 +206,16 @@ export function launchModal({invoice, onPaid}: LaunchModalArgs = {}) {
   store.getState().setModalOpen(true);
 }
 
+/**
+ * Programmatically close the modal
+ */
 export function closeModal() {
   store.getState().setModalOpen(false);
 }
 
+/**
+ * Programmatically disconnect from a user's wallet and remove saved configuration
+ */
 export function disconnect() {
   store.getState().disconnect();
 }
