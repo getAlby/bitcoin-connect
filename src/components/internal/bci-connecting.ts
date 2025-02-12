01a5e79d406d00b1029d83a1d298e64a54d18cc5
Roland Bewick <roland.bewick@gmail.com>
2025-02-12 21:07:25 +0700

    chore: reuse disconnect section

--- src/components/internal/bci-connecting.ts
+++ src/components/internal/bci-connecting.ts
@@ -2,9 +2,8 @@ import {LitElement, html} from 'lit';
 import {withTwind} from '../twind/withTwind';
 import {customElement} from 'lit/decorators.js';
 import {classes} from '../css/classes';
-import {disconnectIcon} from '../icons/disconnectIcon';
-import store from '../../state/store';
 import {waitingIcon} from '../icons/waitingIcon';
+import {disconnectSection} from '../templates/disconnectSection';
 
 @customElement('bci-connecting')
 export class Connecting extends withTwind()(LitElement) {
@@ -15,21 +14,9 @@ export class Connecting extends withTwind()(LitElement) {
         <p class="text-center font-sans ${classes['text-neutral-secondary']}">
           Connecting to wallet...
         </p>
-        <button
-          @click=${this._handleDisconnect}
-          class="flex items-center gap-2 mt-4 px-3 py-2 rounded hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors ${classes[
-            'text-neutral-tertiary'
-          ]}"
-        >
-          ${disconnectIcon}
-          <span class="text-sm">Disconnect</span>
-        </button>
+        ${disconnectSection(undefined)}
       </div>
     `;
-  }
-
-  private _handleDisconnect() {
-    store.getState().disconnect();
   }
 }
 
