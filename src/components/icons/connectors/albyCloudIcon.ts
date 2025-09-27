cea4379c751cbb46cc75aca9d439cc3f32b5be90
Achal Jhawar <achaldps@gmail.com>
2025-09-27 13:05:19 +1000

    fix: update icons to use currentColor for better theming support

--- src/components/icons/connectors/albyCloudIcon.ts
+++ src/components/icons/connectors/albyCloudIcon.ts
@@ -1,5 +1,5 @@
 import {svg} from 'lit';
 
 export const albyCloudIcon = svg`<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7.5 19C4.73858 19 2.5 16.7614 2.5 14C2.5 11.4673 4.38316 9.37436 6.82568 9.04508C7.63649 6.69118 9.87075 5 12.5 5C15.8137 5 18.5 7.68629 18.5 11C20.7091 11 22.5 12.7909 22.5 15C22.5 17.2091 20.7091 19 18.5 19H7.5Z" stroke="#196CE7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7.5 19C4.73858 19 2.5 16.7614 2.5 14C2.5 11.4673 4.38316 9.37436 6.82568 9.04508C7.63649 6.69118 9.87075 5 12.5 5C15.8137 5 18.5 7.68629 18.5 11C20.7091 11 22.5 12.7909 22.5 15C22.5 17.2091 20.7091 19 18.5 19H7.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
